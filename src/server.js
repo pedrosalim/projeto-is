fe3dc5a8a21e5befc8be800244b223685b2b8dfc
Pedro Salim <pedrof.salim@gmail.com>
2020-11-22 18:28:56 -0300

    .

--- src/server.js
+++ src/server.js
@@ -7,7 +7,7 @@ const methodOverride = require('method-override')
 const server = express()
 
 server.use(express.urlencoded({ extended: true }))
-server.use(express.static('public'))
+server.use(express.static(__dirname + 'public'))
 server.use(methodOverride('_method'))
 // server.set('views', path.join(__dirname, "views"))
 // path.resolve(__dirname,'views')
