222d655eea07d43ac7b64e0b523ce24a8501ba87
Pedro Salim <pedrof.salim@gmail.com>
2020-11-22 16:57:23 -0300

    .

--- src/config/db.js
+++ src/config/db.js
@@ -2,7 +2,7 @@ const { Pool } = require('pg')
 
 module.exports = new Pool({
     user: 'pedro',
-    password: "",
+    password: "Fqv3we1785",
     host: "localhost",
     port: 5432,
     database: "videoteste"
